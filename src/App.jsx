9ddaa835ed9d41ff94bb84a9682d8bc76c2bb459
Dii11 <mandimbiarisatad@gmail.com>
2025-05-03 11:53:56 +0300

    init prject

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    < h2  className = "text-2xl font-bold text-blue-800" > Carte Tailwind </ h2 > 
+    )
+}
+
+export default App
