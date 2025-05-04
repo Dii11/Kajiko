a304fbde4db42f9b9da0b758b8f26a40a6c2cb3f
Dii11 <mandimbiarisatad@gmail.com>
2025-05-04 11:19:25 +0300

    init api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
 import './App.scss'
 import LandingPage from './auth/LandingPage'
 import Login from './auth/Login'
 import Register from './auth/Register'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return <Register/>
+  return <Router>
+    <Routes>
+      <Route exact path="/" element={<LandingPage/>}/>
+      <Route exact path="/login" element={<Login/>}/>
+      <Route exact path="/register" element={<Register/>}/>
+    </Routes>
+  </Router>
 }
 
 export default App
